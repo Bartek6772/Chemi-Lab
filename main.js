5027da6c68176b54d4aba39a76eb637a9e951a53
Bartłomiej <67584326+Bartek6772@users.noreply.github.com>
2022-03-08 19:14:12 +0100

    Bugfix
    fix windows status save system nad add required tag to inputs

--- main.js
+++ main.js
@@ -2,14 +2,15 @@ var windows = Array.from(document.getElementsByClassName("window"));
 
 windows.forEach(element => {
 
-    let data = localStorage.getItem(element.dataset.name) == "true";
+    let data = localStorage.getItem(element.dataset.name);
     let isActive = element.classList.contains('window--active');
 
+    console.log(element.dataset.name, isActive);
     
     if (data == null) {
         localStorage.setItem(element.dataset.name, isActive)
     }else{
-        if(data){
+        if(data == "true"){
             if(!isActive) element.classList.add('window--active');
         }else{
             if (isActive) element.classList.remove('window--active');
